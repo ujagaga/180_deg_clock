52a5caddb834d62f759c33b5ae7d039df0559cd3
Rada Berar <ujagaga@gmail.com>
2022-01-05 19:34:15 +0100

    Simplified local time server

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # ESP8266 Semi-mechanical Clock
 
 This is a wall clock based on ESP8266. It uses a simple phone charger for power. 
-The time is synchronized from internet. Hours are displayed using a servo motor that rotates only 180 degrees. Minutes are displayed using two 7 segment LED displays.
+The time is synchronized from local network. Hours are displayed using a servo motor that rotates only 180 degrees. Minutes are displayed using two 7 segment LED displays.
 
 ## Implementation  Details
 
@@ -9,9 +9,14 @@ The hour hand is moved by the servo motor using two gears so that the hand can b
 For hour numbers, you can use anything you like, custom printed shapes, familly photos, wall art, light bulbs,...
 I am using an addressable RGB LED strip in a 3D printed 7 segment display because it only requires one pin for communication, which simplifies the schematic and allows for color change.
 
+## Local time server
+
+Using internet NTP server is accurate, but only shows UTC time. Adjusting for daylight savings can be tricky in an embedded device like ESP8266, so I wrote a simple python script to serve local time on a custom port. It can be deployed on any computer on local network, so the clock can synchronize when ever this computer is online. It's only dependency is Python3.
+
 ## Status
 
 Not yet ready, but soon... If you wish to be notified when it is ready, let me know.
+
 
 ## Contact
 
--- local_time_server/local_time.py
+++ local_time_server/local_time.py
@@ -1,18 +1,42 @@
 #!/usr/bin/env python3
 
-from flask import Flask, request, render_template, Markup, send_from_directory, g, redirect, make_response
+"""
+This is a simple time server to display local time via http get request.
+NTP internet servers only show UTC time and I want the local time for my custom made wall clock.
+An alternative is to install a local NTP server, but running this python script is much simpler
+both for setup and access.
+"""
+
 import time
+from http.server import BaseHTTPRequestHandler, HTTPServer
 
-
-application = Flask(__name__)
 WEB_PORT = 60000
 
 
-@application.route('/')
-def home_page():
-    local_time = time.localtime()
-    return "{}:{}:{}".format(local_time.tm_hour, local_time.tm_min, local_time.tm_sec)
+class ResponseHandler(BaseHTTPRequestHandler):
+    def _set_response(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/plain')
+        self.end_headers()
+
+    def do_GET(self):
+        self._set_response()
+        local_time = time.localtime()
+        self.wfile.write("{}:{}:{}".format(local_time.tm_hour, local_time.tm_min, local_time.tm_sec).encode('utf-8'))
+
+
+def run():
+    httpd = HTTPServer(('0.0.0.0', WEB_PORT), ResponseHandler)
+    print('Starting local time server on port {}.'.format(WEB_PORT))
+
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        pass
+    httpd.server_close()
+
+    print('Stopping local time server')
 
 
 if __name__ == '__main__':
-    application.run(host="0.0.0.0", port=WEB_PORT, debug=True)
+    run()
